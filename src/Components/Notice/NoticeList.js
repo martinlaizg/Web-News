ab3b7f64d486ba146453cfaf218158f345d4bd04
Martin Laiz <martinlaizg@gmail.com>
2017-12-02 21:06:21 +0100

    Notice and Header test

---
+++ src/Components/Notice/NoticeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Notice from './Notice';
+
+class NoticeList extends Component {
+	render() {
+		
+		return (
+			<div></div>
+		);
+	}
+}
+
+export default NoticeList;
