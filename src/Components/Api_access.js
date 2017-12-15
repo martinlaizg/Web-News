4e4398b5a5de626a02f02a257e5a3c4052cd9aaa
Martin Laiz Gómez <martinlaizg@gmail.com>
2017-12-15 17:20:58 +0100

    Changed API to deply API

--- src/Components/Api_access.js
+++ src/Components/Api_access.js
@@ -1,7 +1,7 @@
 
 class Api_access {
 	constructor() {
-		this.url = 'http://localhost:5000'//'https://api-news-martinlaiz.herokuapp.com'
+		this.url = 'https://api-news-martinlaiz.herokuapp.com'
 	}
 
 	getCategories() {
